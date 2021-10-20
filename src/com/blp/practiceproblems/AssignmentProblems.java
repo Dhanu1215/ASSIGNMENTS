0f2ccc741fa3173c58aa49fbb26ba260e88b07f0
Dhanashree hakke <dhanashreehakke7@gmail.com>
2021-10-20 16:00:07 +0530

    Added: welcome message is added

---
+++ src/com/blp/practiceproblems/AssignmentProblems.java
@@ -0,0 +1,8 @@
+package com.blp.practiceproblems;
+
+public class AssignmentProblems {
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to Assignment practice problem");
+    }
+}
